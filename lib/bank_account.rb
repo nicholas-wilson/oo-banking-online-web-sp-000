ae1c05685b6d6d72a67e0ed0b2e5554c28154333
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 03:00:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,4 +7,20 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    puts @balance
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
 end
